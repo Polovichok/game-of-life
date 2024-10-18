4821feaa69260495407cd2f9081ee0091a259066
Pushok <kuzemin.mikhail@gmail.com>
2024-10-18 21:32:49 +0300

    1st version

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/fogleman/gg"
 	"image"
 	"image/color"
@@ -14,29 +15,36 @@ const (
 	length         int = 500
 	width          int = 500
 	squareSideSize int = 25
+	frameNumber    int = 100
 )
 
 func main() {
 	answer := createArray()
 	fillingArray(&answer)
 
-	new_png_file, myImage := createField()
-	drawSquares(&answer, myImage)
+	for frame := 0; frame < frameNumber; frame++ {
 
-	dc := gg.NewContext(length, width)
-	dc.DrawImage(myImage, 0, 0)
+		new_png_file := fmt.Sprintf("e:/prog-projects/go-projects/game-of-life/assets/frame_%d.png", frame)
 
-	DrawLines(myImage, dc)
+		_, myImage := createImage()
+		drawSquares(&answer, myImage)
 
-	myfile, err := os.Create(new_png_file)
-	if err != nil {
-		panic(err)
-	}
+		dc := gg.NewContext(length, width)
+		dc.DrawImage(myImage, 0, 0)
 
-	defer myfile.Close()
+		DrawLines(myImage, dc)
 
-	if err := png.Encode(myfile, dc.Image()); err != nil {
-		panic(err)
+		myfile, err := os.Create(new_png_file)
+		if err != nil {
+			panic(err)
+		}
+
+		defer myfile.Close()
+
+		if err := png.Encode(myfile, dc.Image()); err != nil {
+			panic(err)
+		}
+		fillingNewArray(&answer)
 	}
 }
 
@@ -59,8 +67,8 @@ func fillingArray(array *[width][length]int) {
 	}
 }
 
-func createField() (string, *image.RGBA) {
-	new_png_file := "e:/prog-projects/go-projects/game-of-life/rectangle.png"
+func createImage() (string, *image.RGBA) {
+	new_png_file := "e:/prog-projects/go-projects/game-of-life/assets/rectangle.png"
 	myImage := image.NewRGBA(image.Rect(0, 0, length, width))
 	myGrey := color.RGBA{160, 160, 160, 160}
 	draw.Draw(myImage, myImage.Bounds(), &image.Uniform{myGrey}, image.Point{}, draw.Src)
@@ -92,7 +100,7 @@ func drawSquares(answer *[width][length]int, myImage *image.RGBA) {
 	}
 }
 
-func DrawLines(myImage *image.RGBA, dc *gg.Context){
+func DrawLines(myImage *image.RGBA, dc *gg.Context) {
 	for i := 0; i <= length; i += squareSideSize {
 
 		dc.DrawLine(float64(i), float64(0), float64(i), float64(width))
@@ -100,7 +108,38 @@ func DrawLines(myImage *image.RGBA, dc *gg.Context){
 	for i := 0; i <= width; i += squareSideSize {
 		dc.DrawLine(float64(0), float64(i), float64(length), float64(i))
 	}
-	dc.SetRGB(0,0,0)
+	dc.SetRGB(0, 0, 0)
 	dc.SetLineWidth(1)
 	dc.Stroke()
+}
+
+func fillingNewArray(array *[width][length]int) [width][length]int {
+
+	for i := 1; i < length-1; i++ {
+		for j := 1; j < width-1; j++ {
+			sum := checkNeighbors(array, i, j)
+			if sum == 3 && array[i][j] == 0 {
+				array[i][j] = 1
+			} else if sum == 2 || sum == 3 && array[i][j] == 1 {
+				array[i][j] = 1
+			} else {
+				array[i][j] = 0
+			}
+		}
+	}
+	return *array
+}
+
+func checkNeighbors(array *[width][length]int, i int, j int) int {
+	count := 0
+	for k := i - 1; k <= i+1; k++ {
+		for p := j - 1; p <= j+1; p++ {
+			if k >= 0 && k < width && p >= 0 && p < length {
+				count += array[k][p]
+
+			}
+		}
+	}
+	count -= array[i][j]
+	return count
 }
